fef3d7de3dc4d46dbcaf98074108302f64891303
Rexford Nkansah <seanmavley@gmail.com>
2017-01-09 14:52:53 +0000

    Added Admin Route Guards. Beautified the Admin homepage a bit

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -5,11 +5,13 @@ import { AdminComponent } from './admin.component';
 import { LoginComponent } from './login/login.component';
 import { AuthenticationService } from '../services/auth.service';
 
+import { AuthGuard } from '../services/auth.guard';
+
 const routes: Routes = [
   // Admin page will be LOGGED IN state of the Administrator. If not authenticated
   // Will redirect to Admin/Login URL for authentication
   // The Admin page will be AuthGuarded eventually
-  { path: 'admin', component: AdminComponent },
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
 
   // This login Page will be separate from the Hotspotlogin page for public users 
   // to authenticate in order to use Network.
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,9 +2,11 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'admin', component: AdminComponent }
 ];
 
 export const AppRouting: ModuleWithProviders = RouterModule.forRoot(appRoutes);
